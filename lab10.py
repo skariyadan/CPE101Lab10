47196bcd1ca7714322999695b95c1a8cb7c677c8
Sree Kariyadan <skariyad@calpoly.edu>
2017-06-02 10:22:07 -0700

    initial commit

---
+++ lab10.py
@@ -0,0 +1,15 @@
+# Name:         Sree Kariyadan
+# Course:       CPE 101
+# Instructor:   Nupur Garg
+# Assignment:   Lab10 -- Lab10
+# Term:         Spring 2017
+
+
+import lab10helper
+
+def main():
+    print("Just a simple file.")
+        print(lab10helper.name())
+
+if __name__ == '__main__':
+    main()
---
+++ lab10helper.py
@@ -0,0 +1,9 @@
+# Name:         Sree Kariyadan
+# Course:       CPE 101
+# Instructor:   Nupur Garg
+# Assignment:   Lab10 -- Lab10 Helper
+# Term:         Spring 2017
+
+
+def name():
+    return("Your name")
